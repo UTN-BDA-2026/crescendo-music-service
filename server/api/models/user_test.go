8531d60011be5002ed7a9f537737f53ef2a57692
CodeR4nger <ignabianchi@gmail.com>
2026-05-29 21:48:30 -0300

    Feat: updated test assertion to testify

--- server/api/models/user_test.go
+++ server/api/models/user_test.go
@@ -3,13 +3,9 @@ package models
 import (
 	"testing"
 	"time"
-)
 
-func assertComparison(t *testing.T, reference any, result any) {
-	if result != reference {
-		t.Errorf("Expected= %v; obtained %v", reference, result)
-	}
-}
+	"github.com/stretchr/testify/assert"
+)
 
 func TestCreateUser(t *testing.T) {
 	user := User{
@@ -21,11 +17,12 @@ func TestCreateUser(t *testing.T) {
 		DateOfBirth:       time.Date(1999, 8, 15, 14, 30, 45, 100, time.Local),
 		ProfilePictureUrl: "files/grrs.png",
 	}
-	assertComparison(t, uint(6), user.Id)
-	assertComparison(t, "TestUsername", user.Username)
-	assertComparison(t, "testmail@mail.com", user.Email)
-	assertComparison(t, 0x3556FF, user.PasswordHash)
-	assertComparison(t, time.Date(2024, 4, 23, 14, 30, 45, 0, time.Local), user.RegisterDate)
-	assertComparison(t, time.Date(1999, 8, 15, 14, 30, 45, 100, time.Local), user.DateOfBirth)
-	assertComparison(t, "files/grrs.png", user.ProfilePictureUrl)
+	check := assert.New(t)
+	check.Equal(uint(6), user.Id)
+	check.Equal("TestUsername", user.Username)
+	check.Equal("testmail@mail.com", user.Email)
+	check.Equal(0x3556FF, user.PasswordHash)
+	check.Equal(time.Date(2024, 4, 23, 14, 30, 45, 0, time.Local), user.RegisterDate)
+	check.Equal(time.Date(1999, 8, 15, 14, 30, 45, 100, time.Local), user.DateOfBirth)
+	check.Equal("files/grrs.png", user.ProfilePictureUrl)
 }
